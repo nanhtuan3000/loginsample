2ea380be605a5a3779de29a8c1dd4aefe1ecf15c
tuanna43 <nanhtuan3000@gmail.com>
2023-02-22 23:15:07 +0700

    test

---
+++ src/main/java/com/tuanna43/loginsample/LoginsampleApplication.java
@@ -0,0 +1,13 @@
+package com.tuanna43.loginsample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginsampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LoginsampleApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/tuanna43/loginsample/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.tuanna43.loginsample;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(LoginsampleApplication.class);
+    }
+
+}
